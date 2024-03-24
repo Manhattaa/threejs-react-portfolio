65eb78101e6d6a4e95b021e6c698e34554c6c2e4
Fady Hatta <Fadynhatta@gmail.com>
2024-03-25 00:11:43 +0100

    fixed a typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
@@ -24,7 +24,7 @@ const App = () => {
             }
           />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </main>
   );
 };
